c6f212d70a5fd092836f5f6f54f3582dbba0c8e8
Arthur Unity <apploidyakutsk@gmail.com>
2019-07-06 13:49:15 +0300

    created Games page script

---
+++ jsx/Games.jsx
@@ -0,0 +1,10 @@
+const React = require('react')
+const GameCardsHolder = require('./layouts/GameCardsHolder.jsx')
+
+class Games extends React.Component {
+	render() {
+		return (
+			<div></div>
+		)
+	}
+}
